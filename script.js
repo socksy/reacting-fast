7f24b31792d7c5f28e20b24718b232dde8da7311
Ben Lovell <ben.j.lovell@gmail.com>
2014-09-22 23:51:10 +0100

    Single datum example

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 function getFakeData(offset, limit, callback) {
   var data = [];
+
   for (var i=0; i<limit; i++) {
     var id = offset + i;
     data.push({
@@ -10,17 +11,43 @@ function getFakeData(offset, limit, callback) {
       description: "Description " + id
     });
   }
+
   callback(null, data);
 };
 
-var DataBox = React.createClass({
-    render: function () {
+var Datum = React.createClass({
+    render: function() {
         return (
-           <div>
-           Test
+            <div class="datum">
+            <h2>
+            {this.props.name}
+            </h2>
+            <span>{this.props.children}</span>
+            </div>
+        );
+    }
+});
+
+var DataBox = React.createClass({
+    render: function() {
+        var dataList = this.state.data.map(function(dataPoint) {
+            return (
+                <Datum name={dataPoint.name}>        
+                  {dataPoint.description}
+                </Datum>
+           );
+        });
+        return (
+           <div id="dataBox">
+           {dataList}
            </div>
        );
+    },
+
+    getInitialState: function() {
+        return {data: [{name: "Name 0", description: "Description 0"}]};
     }
+
 });
 
 React.renderComponent(
