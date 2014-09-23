60f2b1f72b1fc5edca652b94bdba8f289e4a039d
Ben Lovell <ben.j.lovell@gmail.com>
2014-09-23 01:15:09 +0100

    Infinite scrolling works.
    There's some hacks here. In particular, we're relying on the element to
    be scrolled as the window — it might be possible to get the element to
    be generalised, but I haven't worked out how to get Node.scrollTop
    to report the correct values.

--- script.js
+++ script.js
@@ -15,10 +15,27 @@ function getFakeData(offset, limit, callback) {
   callback(null, data);
 };
 
+//Each time you call this function, it gets the next 10 data points
+//The purpose of the anonymous function is to provide a closure
+newData = (function() {
+    currentBase = 1;
+    currentStep = 10;
+    return function () {
+        var returnedData = null;
+        getFakeData(currentBase, currentStep, 
+                    function(err, data) {
+                        returnedData = data;
+                    });
+        currentBase += currentStep;
+        return returnedData;
+    }
+})();
+
+//the JSX representation of a single point, easily changed
 var Datum = React.createClass({
     render: function() {
         return (
-            <div class="datum">
+            <div className="datum">
             <h2>
             {this.props.name}
             </h2>
@@ -29,11 +46,28 @@ var Datum = React.createClass({
 });
 
 var DataBox = React.createClass({
+    //DidMount is the function called after it's instantiated into a DOM object
+    componentDidMount: function() {
+        window.addEventListener('scroll', this.updateListener);
+    },
+    
+    updateListener: function() {
+        var node = this.getDOMNode();
+        //scroll height is total scrolled area
+        //inner height is visible area
+        //want threshold to be when the top is a page and a bit away from the end
+        var threshold = node.scrollHeight - (window.innerHeight * 1.1);
+        console.log(threshold + ' ' + window.scrollY)
+        if (window.scrollY > threshold) {
+            this.setState({data: this.state.data.concat(newData())});
+        }
+    },
+
     render: function() {
         var dataList = this.state.data.map(function(dataPoint) {
             return (
                 <Datum name={dataPoint.name}>        
-                  {dataPoint.description}
+                {dataPoint.description}
                 </Datum>
            );
         });
@@ -45,10 +79,11 @@ var DataBox = React.createClass({
     },
 
     getInitialState: function() {
-        return {data: [{name: "Name 0", description: "Description 0"}]};
+        return {data: newData()};
     }
 
 });
+
 
 React.renderComponent(
     <DataBox />,
